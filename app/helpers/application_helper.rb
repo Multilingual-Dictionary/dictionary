3b15646d44eb6ca3a63d9f027a8dd0573cbbdea6
nguyenletien <nguyen_letien@yahoo.com>
2018-10-04 13:27:51 -0700

    ...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Dictionary"
+    base_title = "Từ điển tổng hợp"
     if page_title.empty?
       base_title
     else
