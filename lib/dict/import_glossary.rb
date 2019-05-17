3b15a68c43408d02754cf6c9c7f3d5e854949c11
nguyenletien <nguyen_letien@yahoo.com>
2019-05-17 14:21:30 -0700

    ....

--- lib/dict/import_glossary.rb
+++ lib/dict/import_glossary.rb
@@ -1,10 +1,26 @@
+##
+##	Import excel file -> dictionary
+##
 require_relative "glossary_import.rb"
+
+host=	"localhost" if host==nil
+username="root" if username==nil
+password="letien1512" if password==nil
+database="dictionary_development" if database==nil
+
+puts host
+puts username
+puts password
+puts database
+puts ARGV[0]
+puts ARGV[1]
+
 cb= GlossaryImportCallback.new
 imp=GlossaryImport.new(
-		{"host" =>"localhost",
-		 "username" => "root", 
-		 "password" => "letien1512", 
-		 "database" => "dictionary_development"
+		{"host" =>host,
+		 "username" => username, 
+		 "password" => password, 
+		 "database" => database
 		},
 		cb)
 para = Hash.new 
