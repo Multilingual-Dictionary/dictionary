3c56f1a17e99c0b5c0f933c49695bcf7f3e26337
nguyenletien <nguyen_letien@yahoo.com>
2019-10-25 22:10:42 -0700

    ...

--- app/controllers/admin/config_dicts_controller.rb
+++ app/controllers/admin/config_dicts_controller.rb
@@ -1,7 +1,11 @@
+require './lib/dict/dictionaries_list_export'
+
 class AdminPagesController < ApplicationController
-def config_dicts
+  def config_dicts
 	require_role("ADMIN")
-	printf("CFG-DICTS %s\n",params.inspect())
+	if params[:todo] == "export_list"
+		export_list()
+	end
 	if params[:dict_name] != nil and  params[:dict_name] != ""
 		printf("---\n")
 		@dict_configs = DictConfig.where(
@@ -10,5 +14,16 @@ def config_dicts
 	else
 		@dict_configs = DictConfig.order(priority: :desc, dict_name: :asc) 
     	end
+  end
+  def export_list
+        db_config= YAML::load(File.open('config/database.yml'))
+	exp=DictionaryListExport.new(
+		{"host" =>"localhost",
+                 "username" => db_config[Rails.env]["username"],
+                 "password" =>db_config[Rails.env]["password"],
+                 "database" =>db_config[Rails.env]["database"]
+		})
+	exp.do_export("/tmp/danh_sach_tu_dien.xlsx")
+	send_file "/tmp/danh_sach_tu_dien.xlsx"
   end
 end
